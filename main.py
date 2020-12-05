25848a0dc4061a263554db45f5d817ec884a61d6
life4gal <life4gal@gmail.com>
2020-12-05 17:35:17 +0800

    Added explanation of main.py file

--- main.py
+++ main.py
@@ -1,40 +1,59 @@
+"""
+This file is about the display of the Tetris Game
+"""
+
 import Src.AI as AI
 import Src.Piece as Piece
 import pygame
 import typing
 import random
 
-pygame.init()
-pygame.font.init()
-
+"""
+You can customize the display by changing the following 5 variables
+(SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE, COLUMNS, ROWS), 
+but pay attention to the values of COLUMNS and BLOCK_SIZE. 
+If their product is greater than SCREEN_WIDTH, it will be out of bounds. 
+The same is true for ROWS.
+"""
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 1200
 BLOCK_SIZE = 50
 COLUMNS = 10
 ROWS = 20
+# You should not need to change the following 2 values
 GAME_FIELD_END_X = (SCREEN_WIDTH - BLOCK_SIZE * COLUMNS) // 2 - BLOCK_SIZE + BLOCK_SIZE * COLUMNS
 GAME_FIELD_END_Y = (SCREEN_HEIGHT - BLOCK_SIZE * (ROWS + 1)) // 2 + BLOCK_SIZE * ROWS
 
+# pygame init
+pygame.init()
+pygame.font.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Tetris-AI")
-clock = pygame.time.Clock()
-clock.tick(60)
 font = pygame.font.SysFont("Comic Sans MS", 60)
 
+# ai init
 ai = AI.AI(COLUMNS, ROWS)
 
 
 def get_random_color() -> typing.Tuple[int, int, int]:
+	"""
+	Get a random color, it will affect performance, but just leave it alone, just make it looks good :).
+	:return:
+	"""
 	return random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)
 
 
-def draw_board():
+def draw_board() -> None:
+	"""
+	Show the situation of the board
+	:return: None
+	"""
 	screen.fill((0, 0, 0))
 	for row in range(ROWS):
 		column = 0
 		row_value = ai.tetris.board[row]
 		# after every action we replace all block, so if this row has no block
-		# we think above rows also are
+		# we think the row above also are blank
 		if row_value == 0:
 			return
 		while column < COLUMNS:
@@ -52,7 +71,11 @@ def draw_board():
 			column += 1
 
 
-def draw_score():
+def draw_score() -> None:
+	"""
+	Show current score
+	:return: None
+	"""
 	score = font.render(f"Score: {ai.total_row_removed}", True, get_random_color())
 	rect = score.get_rect()
 	rect.topleft = (SCREEN_WIDTH // 2 - rect.w // 2, (SCREEN_HEIGHT - BLOCK_SIZE * ROWS) // 2)
@@ -60,6 +83,15 @@ def draw_score():
 
 
 def draw_current_piece():
+	"""
+	Show current piece
+
+	Note:
+		we set height to (SCREEN_HEIGHT - BLOCK_SIZE * ROWS) // 2 + BLOCK_SIZE，
+		this means we are BLOCK_SIZE lower than the position where score is displayed,
+		so do not make the font size too big
+	:return: None
+	"""
 	height = (SCREEN_HEIGHT - BLOCK_SIZE * ROWS) // 2 + BLOCK_SIZE
 	# we just need the last character of name
 	name = font.render(f"Current Piece: {ai.current_piece.name[-1]}", True, get_random_color())
